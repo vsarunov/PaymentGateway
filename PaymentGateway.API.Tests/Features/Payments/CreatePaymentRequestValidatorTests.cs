acebf1b88ff6a5dbcab02e454fc60a85f84876bc
Vladislav Sarunov <mrvo788@gmail.com>
2020-02-09 20:20:18 +0200

    Addign test class for payment request validator

---
+++ PaymentGateway.API.Tests/Features/Payments/CreatePaymentRequestValidatorTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentGateway.API.Tests.Features.Payments
+{
+    class CreatePaymentRequestValidatorTests
+    {
+    }
+}
